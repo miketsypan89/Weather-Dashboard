b195a483b3e3dacb549bf72b6e5273a056a519b6
Mike Tsypan <miket@victorytransport.com>
2023-04-03 22:06:21 -0400

    committing unfinished work

--- script.js
+++ script.js
@@ -5,20 +5,28 @@ var inputElement = $(".form-control")
 var apiKey = "6f019b2e75c9e60a83540d4bf5018b75"
 
 function getCityData() {
-    console.log(inputElement)
     var cityName = inputElement[0].value
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=10&appid=${apiKey}`)
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=10&appid=${apiKey}`)
         .then(function (response) {
             return response.json()
         })
         .then(function (data) {
-            fetch(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+            var searchedCityName = data[0].name;
+            var mainCityEl = document.getElementById('main-city-name');
+            mainCityEl.textContent = searchedCityName;
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
                 .then(function (response) {
                     return response.json()
                 })
                 .then(function (data) {
-
+                    console.log(data);
+                    var temp = data.list[0].main.temp
+                    var mainTempEl = document.getElementById('main-current-temp')
+                    mainTempEl.textContent = temp;
                 })
+
         })
 }
 
